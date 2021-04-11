7abceb0539539d0d264d222238de76031048b56b
Cesar <cesar.munuera@gmail.com>
2021-04-11 23:18:19 +0200

    the end ?

--- SOURCES/spi_controller.vhd
+++ SOURCES/spi_controller.vhd
@@ -17,21 +17,21 @@ entity spi_controller is
 end spi_controller;
 
 architecture rtl of spi_controller is
-  signal DATA_SPI_REG    :      std_logic_vector (7 downto 0);  --bloque 1, tiene los 8 primeros bits de data_spi 
-  signal COUNTER_REG     :      unsigned(3 downto 0);           --bloque 2, vector para elegir que bits salen a sdin
-  signal CONT            :      unsigned (6 downto 0);          --contador para bloque 3 obtener fc
-  signal FC              :      std_logic;                      --clock enable para obtener SCLK   
-  signal SCLK_AUX        :      std_logic;                      --Señal auxiliar de SCLK 
-  signal CE              :      std_logic;                      --Señal ClockEnable generada en el Bloque 3
-  signal BUSY            :      std_logic;                      --Señal que nos indica que se esta enviando un dato
-  signal Q               :      std_logic;                      --señal q salida del primer biestable del que obtenemos la señal end_spi
+  signal DATA_SPI_REG    :      std_logic_vector (7 downto 0);  -- Bloque 1, tiene los 8 primeros bits de data_spi.
+  signal COUNTER_REG     :      unsigned(3 downto 0);           -- Bloque 2, vector para elegir que bits salen a SDIN.
+  signal CONT            :      unsigned (6 downto 0);          -- Bloque 3, ontador para bloque 3 obtener FC.
+  signal FC              :      std_logic;                      -- Bloque 3, clock enable para obtener SCLK.  
+  signal SCLK_AUX        :      std_logic;                      -- Bloque 3, señal auxiliar de SCLK.
+  signal CE              :      std_logic;                      -- Bloque 3, señal ClockEnable generada en el Bloque 3.
+  signal BUSY            :      std_logic;                      -- Bloque 3, señal que nos indica que se esta enviando un dato.
+  signal Q               :      std_logic;                      -- Bloque 3, señal "q" salida del primer biestable del que obtenemos la señal END_SPI.
   
 begin
 
 
 -------------------------------------------  BLOQUE 1 ----------------------------------------------
 
--- Obtenemos D_C a partir del bit mas significativo de DATA_SPI mediante biestable D
+-- Obtenemos D_C a partir del bit mas significativo de DATA_SPI mediante biestable D.
 process (CLK, RST) 
     begin
         if (RST = '1') then
@@ -43,7 +43,7 @@ process (CLK, RST)
         end if;
 end process;
 
--- Pasamos la señal DATA_SPI desde el bit 0 al bit 7 con un biestable D. DATA_SPI_OK funciona como CLOCK ENABLE
+-- Pasamos la señal DATA_SPI desde el bit 0 al bit 7 con un biestable D. DATA_SPI_OK funciona como CLOCK ENABLE.
 process (CLK, RST) 
 begin
     if (RST = '1') then
@@ -54,7 +54,6 @@ begin
         end if;
     end if;
 end process;
-
 
 -------------------------------------------  BLOQUE 2 ----------------------------------------------
 
@@ -86,14 +85,14 @@ begin
         SDIN <= '0';
     elsif (CLK'event and CLK = '1') then
         case COUNTER_REG is
-            when "1000"  => SDIN <= DATA_SPI_REG(0);
+            when "1000"  => SDIN <= DATA_SPI_REG(0); -- Cuando COUNTER_REG vale 8 ...
             when "0111"  => SDIN <= DATA_SPI_REG(1);
             when "0110"  => SDIN <= DATA_SPI_REG(2);
             when "0101"  => SDIN <= DATA_SPI_REG(3);
-            when "0100"  => SDIN <= DATA_SPI_REG(4);
+            when "0100"  => SDIN <= DATA_SPI_REG(4); -- Cuando COUNTER_REG vale 4 ...
             when "0011"  => SDIN <= DATA_SPI_REG(5);
             when "0010"  => SDIN <= DATA_SPI_REG(6);
-            when "0001"  => SDIN <= DATA_SPI_REG(7);
+            when "0001"  => SDIN <= DATA_SPI_REG(7); -- Cuando COUNTER_REG vale 1 ...
             when others => null;
         end case;
     end if;
@@ -101,8 +100,7 @@ end process;
 
 
 -------------------------------------------  BLOQUE 3 ----------------------------------------------
-
---BLOQUE 3: Genera CE, SCLK y END_SPI, con la entrada DATA_SPI_OK. 
+--Genera CE, SCLK y END_SPI, con la entrada DATA_SPI_OK. 
 
 -- Generamos FC con PRESCALER; cada N ciclos de CLK se genera un pulso de FC de 10 ns
 -- El factor de división es 6 (del 0 al 5), por que el periodo minimo de SCLK establecido por el protocolo SPI es mayor de 
@@ -127,10 +125,11 @@ begin
     end if;
 end process;
 
--- Generamos SCLK gracias a la señal FC creada en el proceso anterior. Mediante un biestable T, que siempre esta
+-- Generamos SCLK gracias a la señal FC creada en el proceso anterior. Mediante un "biestable T", que siempre esta
 -- activo, conseguimos alternar el valor de SCLK entre 1 y 0 por cada pulso de reloj en el que FC está activo.
 -- Por tanto FC actuara como un CLOCK ENABLE. Por otro lado, necesitamos la señal auxiliar de SCLK ya que esta es solo
--- de salida 
+-- de salida.
+-- No es un biestable T real, ya que cuando BUSY esta inactiva, la señal de SCLK_AUX siempre estara activa.
 process (CLK, RST)
 begin
     if (RST = '1') then
@@ -146,16 +145,14 @@ begin
     end if;
 end process;
 
-SCLK <= SCLK_AUX;
+SCLK <= SCLK_AUX;       -- Asignamos a la señal SCLK, la señal SCLK_AUX, de forma concurrente.
 
 -- En esta sentencia concurrente, generamos la señal de CE que se usa anteriormente. Esta señal estara activa cuando 
 -- SCLK y FC esten activos.
 CE <= FC AND SCLK_AUX;
 
-
-
---Generamos la señal busy, poniendola a 1 cuando DATA_SPI_OK = 1, y poniendola a 0 cuando el contador llega a 8 y cuando
---FC y SCLK_AUX esten a 1. Esta señal busy, se incorpora a el contador, el prescaler  y al biestable que genera SCLK
+-- Generamos la señal BUSY, poniendola a 1 cuando DATA_SPI_OK = 1, y poniendola a 0 cuando el contador llega a 8 y las señales
+-- FC y SCLK_AUX esten a 1. BUSY se incorpora a el contador, el prescaler  y al biestable que genera SCLK.
 process (CLK, RST)
 begin
     if (RST = '1') then
@@ -173,8 +170,8 @@ begin
     end if;
 end process;
 
---Generamos la señal END_SPI, negando primero la señal busy y almacenandola en Q, finalmente obtenemos END_SPI con una puerta NOR,
---cuyas entradas son Q y BUSY
+-- Generamos la señal END_SPI, negando primero la señal BUSY y almacenandola en Q, finalmente obtenemos END_SPI con una puerta NOR,
+-- cuyas entradas son Q y BUSY. Procede de un circuito proporcionado en las diapositivas.
 process(CLK, RST)
 begin
     if (RST = '1') then
@@ -186,7 +183,7 @@ begin
     end if;
 end process;
 
---Finalmente generamos CS negando BUSY
+-- Finalmente generamos CS negando BUSY, para obtener la señal complementaria.
 CS <= NOT BUSY;
         
 end rtl;
