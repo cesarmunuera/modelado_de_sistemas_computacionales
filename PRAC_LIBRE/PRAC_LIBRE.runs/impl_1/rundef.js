6fe4ffa385ec7a83cdd870226531243051f73ee7
Cesar <cesar.munuera@gmail.com>
2021-05-10 20:19:40 +0200

    nada la verdad

--- PRAC_LIBRE/PRAC_LIBRE.runs/impl_1/rundef.js
+++ PRAC_LIBRE/PRAC_LIBRE.runs/impl_1/rundef.js
@@ -8,9 +8,9 @@ var WshShell = new ActiveXObject( "WScript.Shell" );
 var ProcEnv = WshShell.Environment( "Process" );
 var PathVal = ProcEnv("PATH");
 if ( PathVal.length == 0 ) {
-  PathVal = "C:/Xilinx/SDK/2017.4/bin;C:/Xilinx/Vivado/2017.4/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2017.4/ids_lite/ISE/lib/nt64;C:/Xilinx/Vivado/2017.4/bin;";
+  PathVal = "D:/SDK/2017.4/bin;D:/Vivado/2017.4/ids_lite/ISE/bin/nt64;D:/Vivado/2017.4/ids_lite/ISE/lib/nt64;D:/Vivado/2017.4/bin;";
 } else {
-  PathVal = "C:/Xilinx/SDK/2017.4/bin;C:/Xilinx/Vivado/2017.4/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2017.4/ids_lite/ISE/lib/nt64;C:/Xilinx/Vivado/2017.4/bin;" + PathVal;
+  PathVal = "D:/SDK/2017.4/bin;D:/Vivado/2017.4/ids_lite/ISE/bin/nt64;D:/Vivado/2017.4/ids_lite/ISE/lib/nt64;D:/Vivado/2017.4/bin;" + PathVal;
 }
 
 ProcEnv("PATH") = PathVal;
@@ -25,7 +25,7 @@ eval( EAInclude(ISEJScriptLib) );
 // pre-commands:
 ISETouchFile( "init_design", "begin" );
 ISEStep( "vivado",
-         "-log KYPD_controller.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source KYPD_controller.tcl -notrace" );
+         "-log test_oled.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source test_oled.tcl -notrace" );
 
 
 
